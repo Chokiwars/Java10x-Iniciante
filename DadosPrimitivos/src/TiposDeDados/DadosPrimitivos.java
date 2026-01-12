25e6d9898c2417ffe944b5ef7b15d9fb23a3525a
Beatriz <beatrizandreosbezerra1@gmail.com>
2026-01-12 10:25:54 -0300

    First commit

---
+++ DadosPrimitivos/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,17 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+        /*
+        * Dados primitivos - int, double, float, char, boolean, short.
+        * Objetivo da aula: criar um gato - George -
+        * */
+
+        int idade = 12;              // idade do gato em anos
+        double peso = 6.5;         // peso do gato em kg
+        char inicialNome = 'G';     // inicial do nome do gato
+        boolean vacinado = true;    // indica se o gato está vacinado
+
+    }
+}
